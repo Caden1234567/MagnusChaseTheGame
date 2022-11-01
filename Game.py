be43d6f1e8ea0f1a50e7df248d3d7a0960899b8d
NinjaGamingOwner <NinjaGamingOwner@gmail.com>
2022-11-01 10:03:02 -0500

    Create Game.py

---
+++ Game.py
@@ -0,0 +1,4 @@
+print("(System) Welcome To The Game This Is A Story Based Game So Its Kinda Long")
+input("Press Enter To Continue")
+print("(System) This Is A Game Based Off A Book So It Is Hard To Go Off Path")
+input("Press Enter To Continue")
